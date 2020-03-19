334a7bb78862802771cc16f0c105c4d98cf3fe18
CameronAlvarado <cameronapple777@gmail.com>
2020-03-18 20:00:00 -0600

    commmit

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TouchableHighlight } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight,
+} from "react-native";
 
 const GoalItem = (props) => {
-    return (
-        <TouchableOpacity activeOpacity={0.8} onPress={props.delete.bind(this, props.id)} >
-            <View style={styles.list} >
-                <Text style={styles.text}>{props.title}</Text>
-            </View>
-        </TouchableOpacity>
-    )
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={props.delete.bind(this, props.id)}
+    >
+      <View style={styles.list}>
+        <Text style={styles.text}>{props.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    list: {
-        // backgroundColor: 'lightgrey',
-        borderWidth: 1,
-        borderColor: 'dodgerblue',
-        padding: 10,
-        marginVertical: 10,
-        // fontSize: 20  <---- why doesnt this work for childeren???
-      },
-      text: {
-          color: 'white'
-      }
-})
+  list: {
+    backgroundColor: "lightgrey",
+    borderWidth: 1,
+    borderColor: "dodgerblue",
+    padding: 10,
+    marginVertical: 10,
+    // fontSize: 20  <---- why doesnt this work for childeren???
+  },
+  text: {
+    color: "white",
+  },
+});
 
 export default GoalItem;
